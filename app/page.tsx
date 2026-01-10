b1588946d9ae3376554755811d4f0c4be5d27218
Leah Martin <artbyleahmartin@gmail.com>
2026-01-10 15:35:40 -0600

    Add profile image and update Home component

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src ="/profile.jpg"
+          alt="Leah Martin"
+          width={128}
+          height={128}
+          className="rounded-full object-cover"
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
